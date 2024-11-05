de035dd9f1ec185b4de39c84c1d8585fcb0dc9da
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2024-11-06 01:05:07 +0900

    add userSession And userClass

--- src/classes/models/game_class.js
+++ src/classes/models/game_class.js
@@ -3,54 +3,55 @@ import { MAX_PLAYERS } from '../../constants/sessions.js';
 import { errCodes } from '../../utils/error/errCodes.js';
 class Game {
   constructor(gameId) {
-    this._gameId = gameId;
+    this.gameId = gameId;
     // 방 고유 아이디
-    this._users = [];
+    this.users = [];
     // 방을 만든 유저와 상대방을 저장할 배열
-    this._state = 'waiting';
+    this.state = true;
+    // 대기 중이면 true, 게임 중이면 false
   }
 
-  get getGameId() {
-    return this._gameId;
+  getGameId() {
+    return this.gameId;
   }
 
-  // 방의 모든 유저 전달
-  get getAllUsers() {
-    return this._users;
+  // 방의 모든 유저 수 전달
+  getUserCount() {
+    return this.users.length;
   }
 
   addUser(user) {
-    if (this._users.length >= MAX_PLAYERS) {
+    if (this.users.length >= MAX_PLAYERS) {
       // 방에 유저가 2명 이상일 경우 방이 꽉찬 상태라 오류를 뜻한다.
-      throw new CustomErr(errCodes.GAME_FULL_USER, '방에 유저가 꽉 찬 상태 입니다.');
+      //throw new CustomErr(errCodes.GAME_FULL_USER, '방에 유저가 꽉 찬 상태 입니다.');
     }
-    this._users.push(user);
+    this.users.push(user);
     // 방에 유저가 2명 미만일 경우 방에 유저 추가
-    if (this._users.length === MAX_PLAYERS) {
+    if (this.users.length === MAX_PLAYERS) {
       setTimeout(() => {
         this.startGame();
       }, 3000);
     }
   }
 
-  getUserCount() {
-    return this._users.length;
+  isGameStart() {
+    return this.state;
   }
 
   getUser(userId) {
-    return this._users.find((user) => (user.userId = userId));
+    return this.users.find((user) => (user.userId = userId));
     // 해당 방에 유저가 존재하는지 조회
   }
 
   removeUser(userId) {
-    this._users = this._users.filter((user) => user.userId !== userId);
+    this.users = this._users.filter((user) => user.userId !== userId);
   }
 
   startGame() {
-    this._state = 'inProgress';
-    const startPacket = gameStartNotification(this._gameId, Date.now());
+    this.state = false;
+    const startPacket = gameStartNotification(this.gameId, Date.now());
 
-    this._users.forEach((user) => {
+    this.users.forEach((user) => {
       user.getSocket().write(startPacket);
     });
   }
