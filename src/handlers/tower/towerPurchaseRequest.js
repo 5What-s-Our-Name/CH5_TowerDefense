d21752b03ba1355b6a60064ed3574edf99e91329
Tcoat1 <wlsduqwkd8876@naver.com>
2024-11-07 20:50:34 +0900

    update:"타워 구매"

--- src/handlers/tower/towerPurchaseRequest.js
+++ src/handlers/tower/towerPurchaseRequest.js
@@ -1,3 +1,25 @@
-const towerPurchaseRequest = () => {};
+import { PACKET_TYPE } from '../../constants/header.js';
+import { getGameBySocket } from '../../sessions/game_session.js';
+import { createResponse } from '../../utils/response/createResponse.js';
+
+const towerPurchaseRequest = (socket, sequence, payload) => {
+  const gameSession = getGameBySocket(socket);
+  const { user, opponent } = gameSession.getUsers(socket);
+  const { x, y } = payload;
+  const towerId = user.addTower(x, y);
+  const buyTowerResponse = createResponse(
+    PACKET_TYPE.TOWER_PURCHASE_RESPONSE,
+    user.getNextSequence(),
+    { towerId },
+  );
+  user.socket.write(buyTowerResponse);
+
+  const Notification = createResponse(
+    PACKET_TYPE.ADD_ENEMY_TOWER_NOTIFICATION,
+    opponent.getNextSequence(),
+    { towerId, x, y },
+  );
+  opponent.socket.write(Notification);
+};
 
 export default towerPurchaseRequest;
