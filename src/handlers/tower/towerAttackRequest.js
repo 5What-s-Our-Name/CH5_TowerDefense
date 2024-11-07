33ea36fea640ec7a5e56a2aeac00eb903955b456
Tcoat1 <wlsduqwkd8876@naver.com>
2024-11-07 22:22:36 +0900

    update"타워 공격 동기화"

--- src/handlers/tower/towerAttackRequest.js
+++ src/handlers/tower/towerAttackRequest.js
@@ -1,3 +1,19 @@
-const towerAttackRequest = () => {};
+import { getGameBySocket } from '../../sessions/game_session.js';
+import { PACKET_TYPE } from '../../constants/header.js';
+import { createResponse } from '../../utils/response/createResponse.js';
+
+const towerAttackRequest = (socket, sequence, payload) => {
+  const gameSession = getGameBySocket(socket);
+  const { opponent } = gameSession.getUsers(socket);
+  const { towerId, monsterId } = payload;
+  console.log(towerId, monsterId);
+
+  opponent.socket.write(
+    createResponse(PACKET_TYPE.ENEMY_TOWER_ATTACK_NOTIFICATION, opponent.getNextSequence(), {
+      towerId,
+      monsterId,
+    }),
+  );
+};
 
 export default towerAttackRequest;
