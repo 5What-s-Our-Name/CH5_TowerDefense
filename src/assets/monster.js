a76a7ca0024b51f70b2425cbc98380b16df37585
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2024-11-08 17:25:00 +0900

    add monsterInfo

---
+++ src/assets/monster.js
@@ -0,0 +1,22 @@
+export const monsterInfo = [
+  {
+    gold: 100,
+    score: 100,
+  },
+  {
+    gold: 50,
+    score: 50,
+  },
+  {
+    gold: 70,
+    score: 70,
+  },
+  {
+    gold: 150,
+    score: 150,
+  },
+  {
+    gold: 100,
+    score: 100,
+  },
+];
