0998c00da0f2c98784a61f265784ede34dce4adc
Rxoding <neumreum0419@naver.com>
2024-11-08 22:25:51 +0900

    update: "HighScore 생성"

--- src/utils/game/score.js
+++ src/utils/game/score.js
@@ -3,8 +3,8 @@ import { decodeToken } from '../jwt/decodeToken.js';
 export const saveHighScore = async (token, score) => {
   const { userId } = decodeToken(token);
 
-  const query = 'UPDATE User SET high_score = ? WHERE user_id = ?';
-  const [rows] = await pools.USER_DB.query(query, [score, userId]);
+  const query = 'UPDATE User SET high_score = ? WHERE user_id = ? AND high_score < ?';
+  const [rows] = await pools.USER_DB.query(query, [score, userId, score]);
   console.log(rows);
 };
 export const getHighScore = async (userId) => {
