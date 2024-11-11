a4137f349f64274b71b2e0c2e399161cc644ef64
tjdcjf1996 <r_louis@naver.com>
2024-11-11 17:53:04 +0900

    5

--- src/sessions/user_session.js
+++ src/sessions/user_session.js
@@ -4,25 +4,28 @@ import User from '../classes/models/user_class.js';
 // 유저 객체를 유저 세션에 추가
 export const addUser = (socket, token) => {
   const user = new User(socket, token);
-  //token은 User Class에 의해 userId가 될 겁니다.
-  //그래서 토큰을 비교해야한다면 userId로 비교하세요
-  userSessions.push(user);
+  userSessions.set(socket.name, user);
   return user;
 };
 
 export const getUserByUserId = (userId) => {
-  return userSessions.find((user) => user.userId === userId);
+  for (const user of userSessions.values()) {
+    if (user.userId === userId) {
+      return user;
+    }
+  }
 };
 
 export const removeUser = async (socket) => {
-  const index = userSessions.findIndex((user) => user.socket === socket);
-  if (index != -1) {
-    return userSessions.splice(index, 1)[0];
+  const user = userSessions.get(socket.name);
+  if (user) {
+    userSessions.delete(socket.name);
+    return user;
   }
 };
 
 export const getUserBySocket = (socket) => {
-  const user = userSessions.find((user) => user.socket === socket);
+  const user = userSessions.get(socket.name);
   if (!user) {
     console.error('User not found : getUserBySocket');
   }
@@ -30,5 +33,5 @@ export const getUserBySocket = (socket) => {
 };
 
 export const getUserSessions = () => {
-  return userSessions;
+  return [...userSessions.values()];
 };
